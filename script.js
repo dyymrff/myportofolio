f88bdf19543a31eedd62977a04efaa6925bd7414
dyymrff <alhadymaruf@gmail.com>
2024-11-04 21:09:26 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
+  anchor.addEventListener("click", function (e) {
+    e.preventDefault();
+    document.querySelector(this.getAttribute("href")).scrollIntoView({
+      behavior: "smooth",
+    });
+  });
+});
+window.addEventListener("scroll", () => {
+  const header = document.querySelector("header");
+  if (window.scrollY > 50) {
+    header.classList.add("scrolled");
+  } else {
+    header.classList.remove("scrolled");
+  }
+});
+
+let slideIndex = 1;
+showSlides(slideIndex);
